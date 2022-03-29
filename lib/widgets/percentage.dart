dc73b2bb4acb0338ad2d4d17760268a7c1f3fb86
Hadi Al Marzooq <hadialmarzouq@outlook.com>
2022-03-29 23:19:39 +0300

    flighview updated

---
+++ lib/widgets/percentage.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+import 'flight.dart';
+
+class percentage extends StatefulWidget {
+  final flight fl;
+  const percentage({Key? key, required this.fl}) : super(key: key);
+
+  @override
+  State<percentage> createState() => _percentageState(fl);
+}
+
+class _percentageState extends State<percentage> {
+  final flight fl;
+  _percentageState(this.fl);
+  @override
+  String calculateFlDiff() {
+    return (fl.flightTime.difference(DateTime.now()).inHours.toString());
+  }
+
+  double percent() {
+    if (int.parse(calculateFlDiff()) > 3)
+      return 1;
+    else
+      return ((int.parse(calculateFlDiff()) / 300) * 100);
+  }
+
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(15.0),
+      child: new LinearPercentIndicator(
+        width: MediaQuery.of(context).size.width - 50,
+        animation: true,
+        lineHeight: 20.0,
+        animationDuration: 2000,
+        percent: percent(),
+        linearStrokeCap: LinearStrokeCap.roundAll,
+        progressColor: Colors.green[700],
+        barRadius: Radius.circular(10),
+      ),
+    );
+  }
+}
